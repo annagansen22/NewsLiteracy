17385c6a6e3f837ce9e1c92ca14b2458ee3ba20e
unknown <I2.janssen@student.ru.nl>
2018-11-04 18:58:20 +0100

    added movement and basic mapto maaikes project with item system

---
+++ MSDT backup/TestGame2D/Assets/Scripts/ItemField.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemField : MonoBehaviour {
+
+    public Sprite sprite;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+
+	}
+
+}
